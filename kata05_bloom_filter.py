d754fa0dae8db1afd5141b4ba55aa397dfe3c45b
Aravindh <aravindh@valtanix.net>
2017-10-31 11:45:01 +0530

    This implementation uses the hashing technique to implement the bloom filter

--- kata05_bloom_filter.py
+++ kata05_bloom_filter.py
@@ -1,12 +1,10 @@
 dictionay = "words.txt"
 default_hash_dime = 32
-num_of_hashes = 8
+num_of_hashes = 16
 
 from sys import getsizeof
 
 class SpellChecker(object):
-    def populate(self):
-        raise NotImplementedError
     def check(self, word):
         raise NotImplementedError
     def get_size(self):
@@ -15,28 +13,15 @@ class SpellChecker(object):
         not_found = [w for w in words if not self.check(w)]
         return not_found
 
-class NaiveChecker(SpellChecker):
-    def __init__(self):
-        self.words = None
-        super(NaiveChecker, self).__init__()
-    def get_size(self):
-        return getsizeof(self.words)
-    def populate(self):
-        self.words = set(x.strip() for x in open(dictionay).readlines())
-    def check(self, word):
-        return word in self.words
-
 class BloomChecker(SpellChecker):
     def __init__(self, hash_dim=default_hash_dime, num_hashes=num_of_hashes):
         self.hash_dim = hash_dim
         self.num_hashes = num_hashes
-        self.hash = (2 ** self.hash_dim) - 1
+        self.hash = (2 ** self.hash_dim)-1
         super(BloomChecker, self).__init__()
     def get_size(self):
         return self.hash_dim
-    def _hash_word(self, word):
-        pass
-    def populate(self):
+    def hash_word(self):
         self.words = set(x.strip() for x in open(dictionay).readlines())
     def check(self, word):
         return word in self.words
