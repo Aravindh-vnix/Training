01001f85ac0fdfae4bb5ac0bbf1d5520c42b8cba
Aravindh <aravindh@valtanix.net>
2017-11-18 04:03:01 +0530

    Only class and functions are get created. http://codekata.com/kata/kata11-sorting-it-out/

---
+++ sorting_balls.py
@@ -0,0 +1,10 @@
+#sorting balls program in kata11. http://codekata.com/kata/kata11-sorting-it-out/
+class Rack:
+    def new(self):
+        list=[]
+
+    def add(item):
+        list=[]
+        if item not in list:
+            list.append(item)
+            list.sort()
