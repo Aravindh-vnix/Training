4fe51debbe2764c5d7add71c43747448184c8dcd
Aravindh <aravindh@valtanix.net>
2017-11-28 09:59:28 +0530

    The test case for kata11 first part

--- sorting_balls_test.py
+++ sorting_balls_test.py
@@ -4,15 +4,13 @@ from sorting_balls import Rack
 
 class MyTestCase(unittest.TestCase):
     def test(self):
-        rack=Rack.new(self)
-        self.assertEqual([],rack.balls)
-        rack.add(20)
-        self.assertEqual([20],rack.balls)
-        rack.add(10)
-        self.assertEqual([10,20],rack.balls)
-        rack.add(30)
-        self.assertEqual([10,20,30],rack.balls)
-
+        self.assertEqual([],Rack.balls(self))
+        Rack.add(20)
+        self.assertEqual([20],Rack.balls(self))
+        Rack.add(10)
+        self.assertEqual([10,20], Rack.balls(self))
+        Rack.add(30)
+        self.assertEqual([10,20,30], Rack.balls(self))
 
 if __name__ == '__main__':
     unittest.main()
